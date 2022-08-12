4e01197b55351f0a88f953e9afde59f9a094d2f2
BerkayAygul <berkaygithub@gmail.com>
2022-08-12 18:49:38 +0300

    Controller Eklendi

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DigitalGameStore.Data;
+using DigitalGameStore.Models;
+
+namespace DigitalGameStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Products.Include(p => p.Genre).Include(p => p.Publisher);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Include(p => p.Genre)
+                .Include(p => p.Publisher)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "ID");
+            ViewData["PublisherID"] = new SelectList(_context.Publishers, "ID", "ID");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,UnitPrice,UnitsInStock,AgeRating,ReleaseDate,PublisherID,GenreID")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "ID", products.GenreID);
+            ViewData["PublisherID"] = new SelectList(_context.Publishers, "ID", "ID", products.PublisherID);
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "ID", products.GenreID);
+            ViewData["PublisherID"] = new SelectList(_context.Publishers, "ID", "ID", products.PublisherID);
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,UnitPrice,UnitsInStock,AgeRating,ReleaseDate,PublisherID,GenreID")] Products products)
+        {
+            if (id != products.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "ID", products.GenreID);
+            ViewData["PublisherID"] = new SelectList(_context.Publishers, "ID", "ID", products.PublisherID);
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Include(p => p.Genre)
+                .Include(p => p.Publisher)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.ID == id);
+        }
+    }
+}
